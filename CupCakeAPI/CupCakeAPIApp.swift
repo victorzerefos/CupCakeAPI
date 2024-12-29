22b2ff3117a8ce6d87062e3477fe67f47e4ab5c1
Victor Zerefos <victor.zerefos@gmail.com>
2024-12-29 09:41:59 -0300

    Initial Commit

---
+++ CupCakeAPI/CupCakeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeAPIApp.swift
+//  CupCakeAPI
+//
+//  Created by Victor Zerefos on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
